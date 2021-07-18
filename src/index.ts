a9b958dd4a5d6b33843f5ec62b076579e766eedc
hatahata7757 <syouki100241@gmail.com>
2021-07-18 11:33:43 +0900

    feat: #3 inplement commitlintPluginIssueDrivenDev plugin

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Plugin } from "@commitlint/types";
+import subjectIssueNumberEmpty from "./rules/subjectIssueNumberEmpty";
+
+const commitlintPluginIssueDrivenDev: Plugin = {
+  rules: {
+    "subject-issue-number-empty-rule": subjectIssueNumberEmpty,
+  },
+};
+
+export default commitlintPluginIssueDrivenDev;
